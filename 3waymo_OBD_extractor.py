e105720909a2229e1bf64ecc724a756df50f21c5
eyuell <eyuell@gmail.com>
2021-05-27 18:54:59 +0200

    main path method

--- 3waymo_OBD_extractor.py
+++ 3waymo_OBD_extractor.py
@@ -25,17 +25,15 @@ As OBD step
 import os
 import time
 from datetime import timedelta
-
 from util_obd import extract_OBD
 
-#Path where the input and output folders are available
-main_path = '/home/eyuell/Desktop/ForD/WAYMO/'
-source_folder = main_path + 'input/segment/'
-dest_folder = main_path + 'output/'
-annt_dir = dest_folder + 'annot_files/annotations_vehicle/'
+
+def get_main_path():
+    #Path where the input and output folders are available
+    return '/home/eyuell/Desktop/ForD/WAYMO/'
 
 
-def File_names_and_path(source_folder):
+def file_names_and_path(source_folder):
 
     dir_list = os.listdir(source_folder)
     files = list()
@@ -45,7 +43,7 @@ def File_names_and_path(source_folder):
         path = os.path.join(source_folder, directory)
 
         if os.path.isdir(path):
-            files = files + File_names_and_path(path)
+            files = files + file_names_and_path(path)
         else:
             files.append(path)
 
@@ -54,23 +52,30 @@ def File_names_and_path(source_folder):
     return tf_files
 
 
-path = File_names_and_path(source_folder)
+if __name__=="__main__":
 
-print('Extraction process started:')
-start = time.time()
+    main_path = get_main_path()
+    source_folder = main_path + 'input/segment/'
+    dest_folder = main_path + 'output/'
+    annt_dir = dest_folder + 'annot_files/annotations_vehicle/'
 
-pp = 0
-m = 0
-for filename in path:
-    pp += 1
-    print("Working on segment", pp, "of", len(path),"\r")
-    tf_name = filename.split('/')[-1]
-    vid_name = tf_name.split('_')[0].split('-')[1]
-    m = extract_OBD(m, filename, vid_name, annt_dir, pp)
+    path = File_names_and_path(source_folder)
 
-# Concluding 
-end = time.time()
-elapsed = end - start
-t_del = timedelta(seconds=elapsed)
-print('\nNumber of OBD annotations extracted:', m)
-print('Extraction process complete in Duration= {}\n'.format(t_del))
+    print('Extraction process started:')
+    start = time.time()
+
+    pp = 0
+    m = 0
+    for filename in path:
+        pp += 1
+        print("Working on segment", pp, "of", len(path),"\r")
+        tf_name = filename.split('/')[-1]
+        vid_name = tf_name.split('_')[0].split('-')[1]
+        m = extract_OBD(m, filename, vid_name, annt_dir, pp)
+
+    # Concluding 
+    end = time.time()
+    elapsed = end - start
+    t_del = timedelta(seconds=elapsed)
+    print('\nNumber of OBD annotations extracted:', m)
+    print('Extraction process complete in Duration= {}\n'.format(t_del))
